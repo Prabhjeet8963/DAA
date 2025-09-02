cd65ff7aa95560bf5cbb156c8e636aeaf8fb7d3a
PrabhjeetSingh <prabhjeet8963@gmail.com>
2025-09-02 10:26:52 +0530

    rotateMatrixClockwise.c

---
+++ Lectures/ExtraLabCodes/rotateMatrixClockwise.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void rotateMatrixClockwise(int matrix[][3], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - 1 - j];
+            matrix[i][n - 1 - j] = temp;
+        }
+    }
+}
+
+void showMatrix(int matrix[][3], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int n = 3;
+
+    rotateMatrixClockwise(matrix, n);
+    printf("Rotated Matrix:\n");
+    showMatrix(matrix, n);
+
+    return 0;
+}
+
+    printf("Rotated Matrix:\n");
+    showMatrix(matrix, n);
+
+    return 0;
+}
